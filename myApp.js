68cf2074f7d03dcbb7c24939ae4053a854022a3a
ghostofkratos <giftkasonya@gmail.com>
2025-05-19 09:22:47 +0200

    Switching to gitpod or codespaces

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
+const mongoose = require("mongoose");
 
+const uri = "mongodb+srv://psycho_kratos:xvsXbjMKHLSQ5Saw@mycluster.4dgofgv.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster";
 
 let Person;
 
@@ -50,6 +52,8 @@ const queryChain = (done) => {
 
   done(null /*, data*/);
 };
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
